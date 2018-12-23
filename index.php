07767e850052292379231857df782de5f5455957
johnpoint <jahanngauss414@gmail.com>
2018-12-23 11:41:54 +0800

    fix

--- index.php
+++ index.php
@@ -6,24 +6,24 @@ $ip=$_GET['ip'];
 $ipv6=$_GET['ipv6'];
 $hn=$_GET['hostname'];
 $serverid=$_GET['serverid'];
-$what=$_GET['table'];
+$what=$_GET['what'];
 
 if ($key==$APIKEY) {
   $conn = new mysqli($DBhost, $DBusername, $DBpassword, $DBname);
   if ($conn->connect_error) {
-      die("连接失败: " . $conn->connect_error);
+      die("{\"code\":\"500\",\"message\":\"".$conn->connect_error."}");
   }
   if ($do=='add') {
-    $SQL='INSERT INTO'.$table.'(ip,ipv6,hostname,serverid) VALUES (\''.$ip.'\',\''.$ipv6.'\',\''.$hn.'\',\''.$serverid.'\')';
+    $SQL='INSERT INTO '.$what.' (ip,ipv6,hostname,serverid) VALUES (\''.$ip.'\',\''.$ipv6.'\',\''.$hn.'\',\''.$serverid.'\')';
   } elseif ($do=='del') {
-    $SQL='DELETE FROM'.$table.'WHERE serverid='.$serverid;
+    $SQL='DELETE FROM'.$what.'WHERE serverid='.$serverid;
   } /*elseif ($do=='update') {
     // TODO
   }*/
   if ($conn->query($SQL) === TRUE) {
       echo "{\"code\":\"200\"}";
   } else {
-      echo "{\"code\":\"423\",\"message\":\"".$conn->error."\"}";
+      echo "{\"code\":\"423\",\"message\":\"".$conn->error."\",\"SQL\":\"".$SQL."\'}";
   }
   $conn->close();
 } else {
