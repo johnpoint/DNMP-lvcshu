d7ac6e1fd3aacb0ca6784e06772e0c3d25422792
johnpoint <jahanngauss414@gmail.com>
2018-12-25 16:42:37 +0800

    格式调整

--- index.php
+++ index.php
@@ -9,26 +9,25 @@ $serverid=$_GET['serverid'];
 $what=$_GET['what'];
 
 if ($key==$APIKEY) {
-  $conn = new mysqli($DBhost, $DBusername, $DBpassword, $DBname);
-  if ($conn->connect_error) {
-      die("{\"code\":\"500\",\"message\":\"".$conn->connect_error."}");
-  }
-  if ($do=='add') {
-    $SQL='INSERT INTO '.$what.' (ip,ipv6,hostname,serverid) VALUES (\''.$ip.'\',\''.$ipv6.'\',\''.$hn.'\',\''.$serverid.'\')';
-    $back='服务器注册成功';
-  } elseif ($do=='del') {
-    $SQL='DELETE FROM'.$what.'WHERE serverid='.$serverid;
-    $back='服务器删除成功';
-  } /*elseif ($do=='update') {
+    $conn = new mysqli($DBhost, $DBusername, $DBpassword, $DBname);
+    if ($conn->connect_error) {
+        die("{\"code\":\"500\",\"message\":\"".$conn->connect_error."}");
+    }
+    if ($do=='add') {
+        $SQL='INSERT INTO '.$what.' (ip,ipv6,hostname,serverid) VALUES (\''.$ip.'\',\''.$ipv6.'\',\''.$hn.'\',\''.$serverid.'\')';
+        $back='服务器注册成功';
+    } elseif ($do=='del') {
+        $SQL='DELETE FROM'.$what.'WHERE serverid='.$serverid;
+        $back='服务器删除成功';
+    } /*elseif ($do=='update') {
     // TODO
   }*/
-  if ($conn->query($SQL) === TRUE) {
-      echo "{\"code\":\"200\",\"message\":\"".$back."\"}";
-  } else {
-      echo "{\"code\":\"423\",\"message\":\"".$conn->error."\",\"SQL\":\"".$SQL."\"}";
-  }
-  $conn->close();
+    if ($conn->query($SQL) === true) {
+        echo "{\"code\":\"200\",\"message\":\"".$back."\"}";
+    } else {
+        echo "{\"code\":\"423\",\"message\":\"".$conn->error."\",\"SQL\":\"".$SQL."\"}";
+    }
+    $conn->close();
 } else {
-  echo "{\"code\":\"403\"}";
+    echo "{\"code\":\"403\"}";
 }
- ?>
