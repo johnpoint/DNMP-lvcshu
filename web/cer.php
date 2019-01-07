1912d4eea01b81bcc676e5f1b4bce76087a77b24
johnpoint <jahanngauss414@gmail.com>
2019-01-07 22:53:52 +0800

    fix

--- web/cer.php
+++ web/cer.php
@@ -1,5 +1,6 @@
 <?php
 echo '<div class="mdui-container doc-container doc-no-cover">
+<script src="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.2/js/mdui.min.js"></script>
   <h1 class="doc-title mdui-text-color-theme">服务器管理面板 - 证书</h1>
   <div class="doc-chapter">
     <div class="mdui-typo">';
