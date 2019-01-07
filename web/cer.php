2974442ac495c1cab3ec69877312c5c8e567dcb1
johnpoint <jahanngauss414@gmail.com>
2019-01-07 22:51:54 +0800

    fix

--- web/cer.php
+++ web/cer.php
@@ -2,8 +2,8 @@
 echo '<div class="mdui-container doc-container doc-no-cover">
   <h1 class="doc-title mdui-text-color-theme">服务器管理面板 - 证书</h1>
   <div class="doc-chapter">
-    <div class="mdui-typo">'
-    include 'cerlist.php';
+    <div class="mdui-typo">';
+    include_once 'cerlist.php';
 echo '</div>
   </div>
 </div>';
