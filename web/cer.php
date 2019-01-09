a8d2cd2ef50186a2992405559a10ba58111b9aeb
johnpoint <jahanngauss414@gmail.com>
2019-01-09 20:09:56 +0800

    fix

--- web/cer.php
+++ web/cer.php
@@ -2,7 +2,7 @@
 include_once 'config.php';
 if ($_COOKIE["user"] == $usercookie) {
   echo '<div class="mdui-container doc-container doc-no-cover">
-    <h1 class="doc-title mdui-text-color-theme">服务器管理面板 - 证书</h1>
+    <h1 class="doc-title mdui-text-color-theme">服务器管理面板 - 证书检查</h1>
     <div class="doc-chapter">
       <div class="mdui-typo">';
       include_once 'cerlist.php';
--- web/getcer.php
+++ web/getcer.php
@@ -1,8 +1,12 @@
 <?php
 include_once 'config.php';
 if ($_COOKIE["user"] == $usercookie) {
+  echo '<div class="mdui-container doc-container doc-no-cover">
+    <h1 class="doc-title mdui-text-color-theme">服务器管理面板 - 证书分发</h1>
+    <div class="doc-chapter">
+      <div class="mdui-typo">';
     echo '<div class="mdui-panel" mdui-panel>';
-    function listDir($dir)
+    function listDir($dir,$cookie)
     {
         if (is_dir($dir)) {
             //打开目录句柄
@@ -13,16 +17,16 @@ if ($_COOKIE["user"] == $usercookie) {
                         echo '<div class="mdui-panel-item">';
                         echo '<div class="mdui-panel-item-header">'.$file.'</div>';
                         echo '<div class="mdui-panel-item-body">';
-                        listDir($dir."/".$file);
+                        listDir($dir."/".$file,$cookie);
                     } else {
                         if ($file != "." && $file != "..") {
                             echo '<div class="mdui-panel" mdui-panel>';
                             echo '<div class="mdui-panel-item">';
                             echo '<div class="mdui-panel-item-header">'.'<div class="mdui-panel-item-title">'.$file.'</div>'.'<i class="mdui-panel-item-arrow mdui-icon material-icons">keyboard_arrow_down</i>'.'</div>';
                             echo '<div class="mdui-panel-item-body">';
-                            echo 'wget https://center.lvcshu.com/getcerfile.php?file='.$dir.'/'.$myfile.$file;
-                            echo '<br>';
-                            echo 'curl https://center.lvcshu.com/getcerfile.php?file='.$dir.'/'.$myfile.$file;
+                            echo '<p> wget https://center.lvcshu.com/getcerfile.php?file='.$dir.'/'.$myfile.$file.' -H \'cookie: user='.$cookie."'".'</p>';
+                            echo '<p> curl https://center.lvcshu.com/getcerfile.php?file='.$dir.'/'.$myfile.$file.' -H \'cookie: user='.$cookie."'".'</p>';
+                            echo "<a href=".'\'https://center.lvcshu.com/getcerfile.php?file='.$dir.'/'.$myfile.$file."'".'class="mdui-btn mdui-btn-raised mdui-ripple mdui-color-theme-accent">Download</a>';
                             echo '</div></div>';
                         }
                     }
@@ -33,8 +37,11 @@ if ($_COOKIE["user"] == $usercookie) {
             echo $dir . '<br>';
         }
     }
-    listDir("ssl");
+    listDir("ssl",$usercookie);
     echo '</div></div></div></div>';
+    echo '</div>
+      </div>
+    </div>';
 } else {
     header("Location: /index.php");
 }
