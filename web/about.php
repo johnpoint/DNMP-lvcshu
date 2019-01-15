7881b551169474dd02517f6fdc0576f44886d5a6
johnpoint <jahanngauss414@gmail.com>
2019-01-15 22:34:59 +0800

    add

--- web/about.php
+++ web/about.php
@@ -6,6 +6,8 @@ echo '<div class="mdui-container doc-container doc-no-cover">
   <h1 class="doc-title mdui-text-color-theme">服务器管理面板 - 关于</h1>
   <div class="doc-chapter">
     <div class="mdui-typo">
+    <h1>开发者</h1>
+    <li><a href="https://github.com/johnpoint">johnpoint</a></li>
     </div>
   </div>
 </div>';
