f4822e1dba0e34fef3092be140b1028428b017cc
johnpoint <jahanngauss414@gmail.com>
2019-01-28 21:51:45 +0800

    update - 证书自动更新脚本 - 部署脚本进行优化 - 添加自述文件 - 进行命令简化

---
+++ README.md
@@ -0,0 +1,6 @@
+# DNMP-lvcshu
+
+执行：
+```
+wget https://git.lvcshu.com/johnpoint/DNMP-lvcshu/raw/master/DNMP && bash DNMP install && soure ~/.bashrc
+```
--- update-cer.sh
+++ update-cer.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-
+url=''
+cat url.list | while read line
+do
+    cd /web/ssl/auto/$line
+    curl $url$line'/fullchain.cer' -H 'cookie: user='$1 > fullchain.cer
+    curl $url$line'/'$line'.key' -H 'cookie: user='$1 > $line.key
+done
