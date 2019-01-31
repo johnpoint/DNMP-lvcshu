ced995fad4c64265a82f6e2b0766e6fdb8574b7a
johnpoint <jahanngauss414@gmail.com>
2019-01-31 13:12:46 +0800

    update

--- web/serverdash.php
+++ web/serverdash.php
@@ -9,12 +9,31 @@ if ( $vcode == 1) {
       <div class="mdui-typo">
       <div id="serverinfo"></div>';
     $info = serverDbView();
+    echo '<div class="mdui-table-fluid">
+    <table class="mdui-table mdui-table-hoverable">';
+    echo '<thead><tr>
+      <th>#</th>
+      <th>ipv4</th>
+      <th>ipv6</th>
+      <th>hostname</th>
+      <th>nginx</th>
+      <th>php-fpm</th>
+      <th>mysql</th>
+      <th>proxy</th>
+    </tr></thead><tbody>';
     for($i = 0;$i <= $info['num'];$i = $i +1){
-        echo $info['info'][$i]['id'];
-        echo $info['info'][$i]['ipv4'];
-        echo $info['info'][$i]['ipv6'];
-        echo $info['info'][$i]['hostname'];
+        echo '<tr><td>'.$info['info'][$i]['id'].'</td>';
+        echo '<tr><td>'.$info['info'][$i]['ipv4'].'</td>';
+        echo '<tr><td>'.$info['info'][$i]['ipv6'].'</td>';
+        echo '<tr><td>'.$info['info'][$i]['hostname'].'</td>';
+        echo '<tr><td>'.$info['info'][$i]['nginx'].'</td>';
+        echo '<tr><td>'.$info['info'][$i]['php-fpm'].'</td>';
+        echo '<tr><td>'.$info['info'][$i]['mysql'].'</td>';
+        echo '<tr><td>'.$info['info'][$i]['proxy'].'</td></tr>';
     }
+    echo '</tbody>
+    </table>
+  </div>';
     echo '</div>
     </div>';
 } else {
