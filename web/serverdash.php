073eb9d175b2f39e95a2f4675f418027532e2887
johnpoint <jahanngauss414@gmail.com>
2019-02-01 12:48:37 +0800

    update

--- web/serverdash.php
+++ web/serverdash.php
@@ -78,20 +78,25 @@ if ( $vcode == 1) {
             $.ajax({
                 url:'db.php',
                 method: 'GET',
-                data: {do:'repo',ipv4:this.ip,key:this.attributes['0'].localName
-                    ,value:'1-0'}
+                data: {do:'repo',ipv4:this.attributes[4].nodeValue,key:this.attributes['0'].localName,value:'1-0'},
+                success: function () {
+                    this.innerHTML='等待';
+                    this.bgColor='yellow';
+                    location.reload();
+                }
             });
         } else {
             $.ajax({
                 url:'db.php',
                 method: 'GET',
-                data: {do:'repo',ipv4:this.ip,key:this.attributes['0'].localName
-                    ,value:'0-1'}
+                data: {do:'repo',ipv4:this.attributes[4].nodeValue,key:this.attributes['0'].localName,value:'0-1'},
+                success: function () {
+                    this.innerHTML='等待';
+                    this.bgColor='yellow';
+                    location.reload();
+                }
             });
         }
-        this.innerHTML='等待';
-        this.bgColor='yellow';
-        location.reload();
       });
     </script>";
 } else {
