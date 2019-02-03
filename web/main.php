c6383b853f4c856ca061780f59e37bfc37ed1922
johnpoint <jahanngauss414@gmail.com>
2019-02-03 13:40:15 +0800

    u庞大特

--- web/main.php
+++ web/main.php
@@ -9,6 +9,8 @@ echo '  <div class="mdui-container doc-container doc-no-cover">
         <div class="mdui-col mdui-m-b-1">
             <a href="?page=status" class="mdui-btn mdui-btn-block mdui-text-left mdui-ripple">监控</a></div>
         <div class="mdui-col mdui-m-b-1">
+            <a href="?page=status" class="mdui-btn mdui-btn-block mdui-text-left mdui-ripple">管理</a></div>
+        <div class="mdui-col mdui-m-b-1">
             <a href="?page=cer" class="mdui-btn mdui-btn-block mdui-text-left mdui-ripple">证书检查</a></div>
         <div class="mdui-col mdui-m-b-1">
             <a href="?page=getcer" class="mdui-btn mdui-btn-block mdui-text-left mdui-ripple">证书分发</a></div>
