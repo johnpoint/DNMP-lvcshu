2eb4ab4807cb7e4f0acdbe912111ec80d396a2c2
johnpoint <me@lvcshu.info>
2019-02-12 20:53:54 +0800

    fix

--- shell/update-cer.sh
+++ shell/update-cer.sh
@@ -2,10 +2,11 @@
 url=''
 key=''
 cd /web/ssl/auto
-ls >> urllist
-cat urllist | while read line
+ls > ../urllist
+cat ../urllist | while read line
 do
     cd /web/ssl/auto/$line
     curl $url'getcerfile.php?file='$line'/fullchain.cer' -H 'cookie: user='$key > fullchain.cer
     curl $url'getcerfile.php?file='$line'/'$line'.key' -H 'cookie: user='$key > $line.key
 done
+rm /web/ssl/urllist
