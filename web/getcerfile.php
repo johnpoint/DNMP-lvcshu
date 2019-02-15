db137ef2d1f72fa5951ca87a423f82ad4a9e0654
johnpoint <me@lvcshu.info>
2019-02-15 17:23:37 +0800

    fix

--- web/getcerfile.php
+++ web/getcerfile.php
@@ -2,7 +2,7 @@
 include_once 'config.php';
 include_once 'verify.php';
 if ( $vcode == 1) {
-    $myfile = fopen($_GET['file'], "r") or die("Unable to open file!");
+    $myfile = fopen('ssl/'.$_GET['file'], "r") or die("Unable to open file!");
     echo fread($myfile, filesize($_GET['file']));
     fclose($myfile);
 } else {
