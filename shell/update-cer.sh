726260232a1ecb8705acbd777a982c94a25085f9
johnpoint <me@lvcshu.info>
2019-02-16 10:43:15 +0800

    update

--- shell/update-cer.sh
+++ shell/update-cer.sh
@@ -6,7 +6,7 @@ ls > ../urllist
 cat ../urllist | while read line
 do
     cd /web/ssl/auto/$line
-    curl $url'/getcerfile.php?secret='$key'&file='$line'/fullchain.cer' > fullchain.cer
-    curl $url'/getcerfile.php?secret='$key'&file='$line'/'$line'.key' > $line.key
+    curl $url'/getcerfile.php?secret='$key'&file='$line'/fullchain.cer' | sed 's/^[ \t]*//g' > fullchain.cer
+    curl $url'/getcerfile.php?secret='$key'&file='$line'/'$line'.key' | sed 's/^[ \t]*//g' > $line.key
 done
 rm /web/ssl/urllist
