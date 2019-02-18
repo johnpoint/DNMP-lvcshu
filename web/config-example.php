384b1f39bae12256ad178511807b2d8a1fcdecb2
johnpoint <me@lvcshu.info>
2019-02-18 11:38:26 +0800

    项目分离

--- web/config-example.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername='';
-$dbusername='';
-$dbpassword='';
-$dbname='';
-$salt='';
- ?>
--- web/setcookie.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once 'config.php';
-$expire=time()+60*60*3;
-setcookie("user",md5($_POST['user'].$_POST['passwd'].$salt),$expire);
-header("Location: /index.php");
- ?>
